9471571939decf4fa09a9131f819332180d45e2f
Negaihoshi <e222et@gmail.com>
2014-05-29 02:06:13 +0800

    First Initialize

---
+++ mainwindow.cpp
@@ -0,0 +1,70 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QFile"
+#include "QFileDialog"
+#include "QMessageBox"
+#include "QTextStream"
+#include "QStandardItemModel"
+#include "QTableWidget"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QStandardItemModel *model = new QStandardItemModel(2,3,this); //2 Rows and 3 Columns
+    model->setHorizontalHeaderItem(0, new QStandardItem(QString("Column1 Header")));
+    model->setHorizontalHeaderItem(1, new QStandardItem(QString("Column2 Header")));
+    model->setHorizontalHeaderItem(2, new QStandardItem(QString("Column3 Header")));
+    ui->tableView->setModel(model);
+    QStandardItem *firstRow = new QStandardItem(QString("ColumnValue"));
+    model->setItem(0,0,firstRow);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    qApp->quit();
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QString(),
+            tr("Text Files (*.txt);;C++ Files (*.cpp *.h)"));
+
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::ReadOnly)) {
+            QMessageBox::critical(this, tr("Error"), tr("Could not open file"));
+            return;
+        }
+        QTextStream in(&file);
+        ui->textEdit->setText(in.readAll());
+        file.close();
+    }
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), QString(),
+            tr("Text Files (*.txt);;C++ Files (*.cpp *.h)"));
+
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::WriteOnly)) {
+            // error message
+        } else {
+            QTextStream stream(&file);
+            stream << ui->textEdit->toPlainText();
+            stream.flush();
+            file.close();
+        }
+    }
+}
+
