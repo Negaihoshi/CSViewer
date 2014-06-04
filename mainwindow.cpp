113d09a4c43527a51c59ce4ad22bdef51108d0da
Negaihoshi <e222et@gmail.com>
2014-06-05 03:40:24 +0800

    update Table Input

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,25 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
 #include "QFile"
 #include "QFileDialog"
 #include "QMessageBox"
 #include "QTextStream"
 #include "QStandardItemModel"
 #include "QTableWidget"
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    QStandardItemModel *model = new QStandardItemModel(2,3,this); //2 Rows and 3 Columns
-    model->setHorizontalHeaderItem(0, new QStandardItem(QString("Column1 Header")));
-    model->setHorizontalHeaderItem(1, new QStandardItem(QString("Column2 Header")));
-    model->setHorizontalHeaderItem(2, new QStandardItem(QString("Column3 Header")));
-    ui->tableView->setModel(model);
-    QStandardItem *firstRow = new QStandardItem(QString("ColumnValue"));
-    model->setItem(0,0,firstRow);
 }
 
 MainWindow::~MainWindow()
@@ -36,7 +30,7 @@ void MainWindow::on_pushButton_clicked()
 void MainWindow::on_actionOpen_triggered()
 {
     QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QString(),
-            tr("Text Files (*.txt);;C++ Files (*.cpp *.h)"));
+            tr("All Files (*);;Text Files (*.txt);;C++ Files (*.cpp *.h);;CSV Files (*.csv)"));
 
     if (!fileName.isEmpty()) {
         QFile file(fileName);
@@ -44,8 +38,32 @@ void MainWindow::on_actionOpen_triggered()
             QMessageBox::critical(this, tr("Error"), tr("Could not open file"));
             return;
         }
+
         QTextStream in(&file);
-        ui->textEdit->setText(in.readAll());
+        //in.setCodec("UTF-8");
+        QString line = in.readLine();
+        int lineCount = 0;
+        while (!line.isNull()) {
+            lineCount++;
+        }
+
+        QStringList fileParts = in.readLine().split(",");
+        int list = fileParts.length();
+        //ui->textEdit->setText(in.readLine());
+
+        QStandardItemModel *model = new QStandardItemModel(2,list,this); //2 Rows and 3 Columns
+        for(int count2=0;count2<lineCount;count2++){
+
+            for(int count = 0; count < list;count++){
+                model->setHorizontalHeaderItem(count, new QStandardItem(QString(fileParts[count])));
+            }
+        }
+        //model->setHorizontalHeaderItem(0, new QStandardItem(QString(fileParts[0])));
+        //model->setHorizontalHeaderItem(1, new QStandardItem(QString("Column2 Header")));
+        //model->setHorizontalHeaderItem(2, new QStandardItem(QString("Column3 Header")));
+        ui->tableView->setModel(model);
+        QStandardItem *firstRow = new QStandardItem(QString("ColumnValue"));
+        model->setItem(0,0,firstRow);
         file.close();
     }
 }
@@ -61,8 +79,6 @@ void MainWindow::on_actionSave_triggered()
             // error message
         } else {
             QTextStream stream(&file);
-            stream << ui->textEdit->toPlainText();
-            stream.flush();
             file.close();
         }
     }
