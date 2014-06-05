5107bcd6c92c91b2fec864d38dd4b6a22f5256d4
Negaihoshi <e222et@gmail.com>
2014-06-05 11:50:32 +0800

    Table load function Finish

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,32 +38,32 @@ void MainWindow::on_actionOpen_triggered()
             QMessageBox::critical(this, tr("Error"), tr("Could not open file"));
             return;
         }
-
         QTextStream in(&file);
-        //in.setCodec("UTF-8");
-        QString line = in.readLine();
-        int lineCount = 0;
-        while (!line.isNull()) {
-            lineCount++;
-        }
 
-        QStringList fileParts = in.readLine().split(",");
+        //in.setCodec("UTF-8");
+
+        QStringList fileLine = in.readAll().split("\n");
+        QStringList fileParts = fileLine[0].split(",");
+        int fileLineNum = fileLine.length();
+
         int list = fileParts.length();
         //ui->textEdit->setText(in.readLine());
 
-        QStandardItemModel *model = new QStandardItemModel(2,list,this); //2 Rows and 3 Columns
-        for(int count2=0;count2<lineCount;count2++){
+        QStandardItemModel *model = new QStandardItemModel(fileLineNum-1,list,this); //2 Rows and 3 Columns
 
+        for(int count = 0; count < list;count++){
+            model->setHorizontalHeaderItem(count, new QStandardItem(QString(fileParts[count])));
+        }
+        ui->tableView->setModel(model);
+
+        for(int count2 = 1 ; count2 < fileLineNum ; count2++){
+            QStringList fileParts2 = fileLine[count2].split(",");
             for(int count = 0; count < list;count++){
-                model->setHorizontalHeaderItem(count, new QStandardItem(QString(fileParts[count])));
+                QStandardItem *firstRow = new QStandardItem(QString(fileParts2[count]));
+                model->setItem(count2-1,count,firstRow);
             }
         }
-        //model->setHorizontalHeaderItem(0, new QStandardItem(QString(fileParts[0])));
-        //model->setHorizontalHeaderItem(1, new QStandardItem(QString("Column2 Header")));
-        //model->setHorizontalHeaderItem(2, new QStandardItem(QString("Column3 Header")));
-        ui->tableView->setModel(model);
-        QStandardItem *firstRow = new QStandardItem(QString("ColumnValue"));
-        model->setItem(0,0,firstRow);
+        ui->tableView->resizeColumnsToContents();
         file.close();
     }
 }
