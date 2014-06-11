5d8b25a12adb793bb6dd89b843eb19654c299e7f
Negaihoshi <e222et@gmail.com>
2014-06-12 01:45:53 +0800

    Finish Save Function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 QStandardItemModel *model = new QStandardItemModel();
+QStringList Header;
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -59,6 +60,7 @@ void MainWindow::on_actionOpen_triggered()
 
         for(int count = 0; count < list;count++){
             model->setHorizontalHeaderItem(count, new QStandardItem(QString(fileParts[count])));
+            Header<<fileParts[count];
         }
         ui->tableView->setModel(model);
 
@@ -98,7 +100,23 @@ void MainWindow::on_actionSave_triggered()
             QTextStream stream(&file);
             QStandardItemModel *model = qobject_cast<QStandardItemModel *>(ui->tableView->model());
             //model->data(model->index(0,0));
-            stream << model->data(model->index(1,1)).toString();
+            qint32 RowMax = model->rowCount();
+            qint32 ColumnMax = model->columnCount();
+            stream << Header.join(",");
+            stream << "\r\n";
+            QStringList csvData;
+            for(int count  = 0; count < RowMax;count++){
+                for(int count2  = 0; count2 < ColumnMax;count2++){
+                    csvData << model->data(model->index(count,count2)).toString();
+                    if(count2==ColumnMax-1){
+                        stream << csvData.join(",") << "\r\n";
+                        csvData.clear();
+                    }
+                }
+            }
+
+            //stream << model->data(model->index(count,count2)).toString();
+            stream << RowMax << "/" << ColumnMax;
             //stream << model->item(1,1).toString();
 
             //stream << ui->tableView->model()->data(model->index(1, 1));
@@ -132,3 +150,93 @@ void MainWindow::on_actionSave_triggered()
     */
 }
 
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), QString(),
+            tr("CSV Files (*.csv);;JSON Files (*.json)"));
+
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::WriteOnly)) {
+            // error message
+        } else {
+            QTextStream stream(&file);
+            QStandardItemModel *model = qobject_cast<QStandardItemModel *>(ui->tableView->model());
+            //model->data(model->index(0,0));
+            qint32 RowMax = model->rowCount();
+            qint32 ColumnMax = model->columnCount();
+            stream << Header.join(",");
+            stream << "\r\n";
+            QStringList csvData;
+            for(int count  = 0; count < RowMax;count++){
+                for(int count2  = 0; count2 < ColumnMax;count2++){
+                    csvData << model->data(model->index(count,count2)).toString();
+                    if(count2==ColumnMax-1){
+                        stream << csvData.join(",") << "\r\n";
+                        csvData.clear();
+                    }
+                }
+            }
+
+            //stream << model->data(model->index(count,count2)).toString();
+            stream << RowMax << "/" << ColumnMax;
+            //stream << model->item(1,1).toString();
+
+            //stream << ui->tableView->model()->data(model->index(1, 1));
+            //stream << ui->tableView->model()->data();
+            file.close();
+        }
+    }
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QString(),
+            tr("All Files (*);;Text Files (*.txt);;C++ Files (*.cpp *.h);;CSV Files (*.csv)"));
+
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::ReadOnly)) {
+            QMessageBox::critical(this, tr("Error"), tr("Could not open file"));
+            return;
+        }
+        QTextStream in(&file);
+
+        //in.setCodec("UTF-8");
+
+        QStringList fileLine = in.readAll().split("\n");
+        QStringList fileParts = fileLine[0].split(",");
+        int fileLineNum = fileLine.length();
+
+        int list = fileParts.length();
+        //ui->textEdit->setText(in.readLine());
+
+        QStandardItemModel *model = new QStandardItemModel(fileLineNum-1,list,this); //2 Rows and 3 Columns
+
+        for(int count = 0; count < list;count++){
+            model->setHorizontalHeaderItem(count, new QStandardItem(QString(fileParts[count])));
+            Header<<fileParts[count];
+        }
+        ui->tableView->setModel(model);
+
+        for(int count2 = 1 ; count2 < fileLineNum ; count2++){
+            QStringList fileParts2 = fileLine[count2].split(",");
+            for(int count = 0; count < list;count++){
+                QStandardItem *firstRow = new QStandardItem(QString(fileParts2[count]));
+                model->setItem(count2-1,count,firstRow);
+            }
+        }
+        //Resizes all columns based on the size hints of the delegate used to render each item in the columns.
+        ui->tableView->resizeColumnsToContents();
+        //Set Visible
+        ui->tableView->verticalHeader()->setVisible(false);
+
+        QStringList fileNameList = fileName.split("/");
+        int fileNameListNum = fileNameList.length();
+        QString newName;
+        newName = "CSViewer | " + fileNameList[fileNameListNum-1];
+        this->setWindowTitle(newName);
+        file.close();
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -22,6 +22,10 @@ private slots:
 
     void on_actionSave_triggered();
 
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
